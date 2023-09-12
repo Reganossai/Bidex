b70f765e8c06c7139774819dd225cf6e6c1c21e5
Reagan <ossaireagano@gmail.com>
2023-09-12 12:14:33 +0100

    update

--- src/components/PhotoUploader.jsx
+++ src/components/PhotoUploader.jsx
@@ -27,48 +27,54 @@ const PhotoUploader = () => {
 
   return (
     <div>
-    <div className='take-upload-photo'>
-      <div>
-        <button className="take-photo" onClick={handleStartWebcam}>Capture Photo</button>
-      </div>
-    </div>
+      {webcamEnabled ? (
+        <div>
+          <Camera
+            onTakePhoto={(dataUri) => {
+              handleTakePhoto(dataUri);
+            }}
+          />
+ 
+        </div>
+      ) : (
+        <div className='take-upload-photo'>
+           {photo && (
+            <div>
+              <h2>Selected Photo:</h2>
+              <img src={photo} alt="Selected" style={{ maxWidth: '100%' }} />
+            </div>
+          )}
+          {!photo && (
+             <Camera
+             onTakePhoto={(dataUri) => {
+               handleTakePhoto(dataUri);
+             }}
+           />
+          )}
 
-    {webcamEnabled && (
-      <div>
-        <Camera
-          onTakePhoto={(dataUri) => {
-            handleTakePhoto(dataUri);
-          }}
-        />
-      </div>
-    )}
-
-    {photo && (
-      <div>
-        <h2>Selected Photo:</h2>
-        <img src={photo} alt='Selected' style={{ maxWidth: '100%' }} />
-      </div>
-    )}
-
-    <Dropzone onDrop={handleDrop}>
-      {({ getRootProps, getInputProps }) => (
-        <div {...getRootProps()} style={dropzoneStyles}>
-          <input {...getInputProps()} />
-          <p className='upload-photo'>Upload ID image</p>
+          <Dropzone onDrop={handleDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()} style={dropzoneStyles}>
+                <input {...getInputProps()} />
+                <p className='upload-photo'>Upload ID image</p>
+              </div>
+            )}
+          </Dropzone>
+         
         </div>
       )}
-    </Dropzone>
-  </div>
-);
-};
+    </div>
+  );
+}
+
 
 const dropzoneStyles = {
-border: 'none',
-borderRadius: '4px',
-padding: '20px',
-textAlign: 'center',
-cursor: 'pointer',
-width: '500px',
-};
+    border: 'none',
+    borderRadius: '4px',
+    padding: '20px',
+    textAlign: 'center',
+    cursor: 'pointer',
+    width: '500px'
+  };
 
 export default PhotoUploader;
